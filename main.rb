8b009f4f447144cebb8d5a568a69a144b8a5de4e
levshits <levshits.l@gmail.com>
2014-09-22 01:28:16 +0300

    Init

---
+++ main.rb
@@ -0,0 +1,81 @@
+require 'fox16'
+require_relative 'slae'
+include Fox
+class
+Main < FXMainWindow
+  def
+  initialize(app)
+    super(app, "DIVM", :width => 500, :height => 400)
+    add_controls
+  end
+  def
+  create
+    super
+    show(PLACEMENT_SCREEN)
+  end
+  private
+  def add_controls
+      controls_frame = FXHorizontalFrame.new(self,:opts=>LAYOUT_FILL_X)
+      FXLabel.new(controls_frame,'Dimension')
+      dimensions_edit = FXTextField.new(controls_frame,5 , :opts=>TEXTFIELD_INTEGER)
+      enter_button = FXButton.new(controls_frame,'Enter',:opts=>BUTTON_NORMAL)
+      enter_button.connect(SEL_COMMAND) do
+        enter_button_click(dimensions_edit.text.to_i)
+        end
+      solve_button = FXButton.new(controls_frame,'Solve',:opts=>BUTTON_NORMAL|LAYOUT_RIGHT)
+      method_box = FXComboBox.new(controls_frame,15,:opts=>COMBOBOX_STATIC|FRAME_SUNKEN|FRAME_THICK|LAYOUT_RIGHT)
+      method_box.fillItems ['Gauss','Holetsky']
+      solve_button.connect(SEL_COMMAND) do
+        solve_button_click(method_box.currentItem)
+      end
+      FXLabel.new(controls_frame,'Method of solving',:opts=>LAYOUT_RIGHT)
+      grid_frame = FXHorizontalFrame.new(self,:opts=>LAYOUT_FILL_X|LAYOUT_FILL_Y)
+      @grid = FXTable.new(grid_frame,:opts=>LAYOUT_FILL_X|LAYOUT_FILL_Y)
+  end
+  def enter_button_click(dimensions)
+    p "enter" +dimensions.to_s
+    @grid.clearItems
+    @grid.insertColumns(0,dimensions+2)
+    @grid.insertRows(0,dimensions)
+    (0...@grid.numRows).each { |i| @grid.setRowText(i,(i+1).to_s+' Equation')}
+    (0...@grid.numColumns-2).each { |i| @grid.setColumnText(i,'x'+(i+1).to_s)}
+    @grid.setColumnText(dimensions+1,'result')
+    @grid.setColumnText(dimensions,'b')
+
+  end
+  def solve_button_click(variant)
+    if @grid.numColumns != 0
+      matrix = Array.new(@grid.numRows){Array.new(@grid.numRows+1,0)}
+      (0...@grid.numRows).each{|i|
+        (0...@grid.numRows+1).each{|j|
+          matrix[i][j] = @grid.getItem(i,j).to_s.to_f}}
+      (0...@grid.numRows).each{|i| @grid.setItemText(i,@grid.numRows+1,'')}
+      matrix = matrix.map {|array| Vector[*array]}
+      slae = Slae.new(matrix)
+      result = 0
+      case variant
+        when(0)
+          result = slae.solve_by_gauss
+        when(1)
+          result = slae.solve_by_holetsky
+
+      end
+      p result
+      case result
+        when(1)
+          FXMessageBox.new(self, 'Result','Unlimited count of solutions',:opts=>MBOX_OK).execute
+        when(2)
+          FXMessageBox.new(self, 'Result','No solution!',:opts=>MBOX_OK).execute
+        when(3)
+          (0...@grid.numRows).each{|i| @grid.setItemText(i,@grid.numRows+1,slae.get_slae(i,-1).round(3).to_s)}
+          FXMessageBox.new(self, 'Result','The system has successfully solved!',:opts=>MBOX_OK).execute
+        when(4)
+          FXMessageBox.new(self, 'Result',"System can't be solved by Holetsky method!",:opts=>MBOX_OK).execute
+      end
+    end
+  end
+end
+app = FXApp.new
+Main.new(app)
+app.create
+app.run
---
+++ slae.rb
@@ -0,0 +1,109 @@
+require 'matrix'
+require 'mathn'
+class Vector
+  def /(arg)
+    self * (1 / arg)
+  end
+end
+class Slae
+
+  def initialize(n)
+    @slae = n
+  end
+  def get_slae(i,j)
+    @slae[i][j]
+  end
+  def get_full_table
+    @slae
+  end
+  def get_size
+    @slae.size
+  end
+  def solve_by_gauss
+    (0...@slae.size).each{ |i|
+      max_index = i
+      max_element = @slae[i][i]
+      (i...@slae.size).each{|j| if @slae[j][i]>max_element
+                                  max_element=@slae[j][i]
+                                  max_index = j
+                                end}
+      if max_index!=i
+        temp = @slae[i]
+        @slae[i] = @slae[max_index]
+        @slae[max_index] = temp
+      end
+      if @slae[i][i]!=0
+       @slae[i] /= @slae[i][i]
+       #p @slae
+       (i+1...@slae.size).each{ |j| @slae[j] -= @slae[i] * @slae[j][i] }
+       #p @slae
+     end
+    }
+    #p @slae
+    is_unlimited = false
+    #p @slae
+    (1...@slae.size).to_a.reverse.each{ |i|
+      (0...i).each{ |j| @slae[j] -= @slae[i] * @slae[j][i] }
+      if @slae[i][i]==0
+        if @slae[i][i] == @slae[i][-1]
+          is_unlimited = true
+        else
+          (0...@slae.size).each{|j|
+            if @slae[i][j]!=0
+              return 1
+            end}
+          return 2
+        end
+      end}
+    if is_unlimited
+      return 1
+    end
+    3
+  end
+  def solve_by_holetsky
+    (0...@slae.size).each{ |i|
+      (i...@slae.size).each { |j|
+        if @slae[i][j]!=@slae[j][i]
+          return 4
+        end}}
+    l_matrix = Array.new(@slae.size){Array.new(@slae.size+1,0)}
+
+    (0...@slae.size).each{|i|
+      (0...i).each{ |j|
+        temp=0
+        (0...j).each{ |k|
+          temp+=l_matrix[i][k]*l_matrix[j][k]
+        }
+        if l_matrix[j][j]==0
+          return 4
+        end
+        l_matrix[i][j] = (@slae[i][j] - temp)/l_matrix[j][j]
+      }
+
+      temp = @slae[i][i]
+      (0...i).each{ |k| temp-=l_matrix[i][k]*l_matrix[i][k]}
+      l_matrix[i][i] = Math.sqrt(temp)
+    }
+    #p l_matrix
+    lt_matrix = Array.new(@slae.size){Array.new(@slae.size+1,0)}
+    (0...@slae.size).each{ |i| (0...@slae.size).each{|j|
+      lt_matrix[i][j] = l_matrix[j][i]
+      if l_matrix[j][i].is_a?(Complex)
+        return 4
+      end}}
+    (0...@slae.size).each{|i| l_matrix[i][-1]=@slae[i][-1]}
+    l_matrix  = l_matrix.map{ |array| Vector[*array]}
+    buffer_slae = Slae.new(l_matrix)
+    if buffer_slae.solve_by_gauss != 3
+      return 4
+    end
+    (0...@slae.size).each{|i| lt_matrix[i][-1] = buffer_slae.get_slae(i, -1)}
+    lt_matrix  = lt_matrix.map{ |array| Vector[*array]}
+    buffer_slae = Slae.new(lt_matrix)
+    if buffer_slae.solve_by_gauss != 3
+      return 4
+    end
+    @slae = buffer_slae.get_full_table
+    3
+  end
+end
